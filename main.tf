cbc120bade3a14f4a85dee7338132721385414f0
Kunal Verma <vkunal321@gmail.com>
2022-12-27 11:24:20 +0530

    refractor structure

--- main.tf
+++ main.tf
@@ -1,25 +1,88 @@
-terraform {}
+# Creating a Data source for the VPC:
+data "aws_vpc" "main" {
+  id = var.vpc_id
+}
 
-# Provider info goes here:
-provider "aws" {
-  region = "us-east-1"
+data "template_file" "user_data" {
+  template = file("${abspath(path.module)}/userdata.yaml")
+}
+
+# Resource - "aws_security_group" (to configure the instance security settings such as inbound ports etc.)
+resource "aws_security_group" "sg_my_server" {
+  name        = "sg_my_server"
+  description = "MyServer Security Group"
+  vpc_id      = data.aws_vpc.main.id
+
+    # in-coming traffic or request configuration
+  ingress = [
+    {
+        description      = "HTTP"
+        from_port        = 80
+        to_port          = 80
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = [] # default vpc being used
+
+        # A few params required to be put in ingress:
+        prefix_list_ids = []
+        security_groups = []
+        self = true
+    },
+    {
+        description      = "SSH"
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = [var.my_ip_with_cidr] # specifying host's IP address here
+        ipv6_cidr_blocks = [] # default vpc being used
+
+        # A few params required to be put in ingress:
+        prefix_list_ids = []
+        security_groups = []
+        self = true
+    }
+  ]
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"] # from anywhere
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = var.public_key
+}
+
+# Data source for AMI:
+data "aws_ami" "amazon-linux-2" {
+  provider = aws
+  most_recent      = true
+  owners           = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 
-# Importing the module here:
-module "apache" {
-  source = ".//terraform-aws-apache-example"
+# Creating an aws ec2 instance:
+resource "aws_instance" "my_server" {
+  ami           = "${data.aws_ami.amazon-linux-2.id}"
+  instance_type = var.instance_type
+  key_name = "${aws_key_pair.deployer.key_name}"
 
-  # Giving the variable values here:
-  vpc_id = "vpc-0155d583e322e06ee"
-  my_ip_with_cidr = "45.127.45.236/32"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqPf7oxnoW97qxWIxcypCAGyt1cLnsFRfGbCVFIXsVBplmu2MvW4g8UhBHl1xwiU3rGQiCsnfVUcfpQ06x3z/HzQvhiqCeqDdDdcOp8rUoYHtu2YT1n1oA6CgKBEv+b2ma6FELsl5xmqkuUiSTLxfoIVzhykP93S6ZBdCjFJWL1pqoQNMjRpuZpdYTNVw21LRaYkTnu9X964pll2oNmvuRJ32eymY80gSONX/UGY/2a60GWuvBV+5tVuSk2aLWM/gqWplfuH7V13p4zDGY0SbTTBOAJ3EgMk1bC+LAVCVbhgrxbl8f2xQSmEvrEDwdvUTjNpuKuKdR4v5yzhF6VoOybsCYfmiuxudkeOEDoTwErjGLbwqu7WPqhl0ao8RaTCdd0omQbKkc9N7JSqYxrNYJcREI1DzCUpbHmbHVcRDOJEVVnWbC8O3XhxbdEUVyKT3MMD1O2tyfI/mAkHbR16HNC2U6fNVT2NcP8NtFdW8/3CsWQ2XNjQoCUycbYu94k98= kunalverma@Kunals-MacBook-Air.local"
-  instance_type = "t2.nano"
-  server_name = "Apache-Example-Server"
-}
+  vpc_security_group_ids = [ aws_security_group.sg_my_server.id ] 
 
-# Outputs:
+  user_data = data.template_file.user_data.rendered
 
-output "public_ip"{
-    value = module.apache.public_id
+  tags = {
+    Name = var.server_name
+  }
 }
